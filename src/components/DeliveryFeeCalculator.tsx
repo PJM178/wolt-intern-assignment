ed84595aa1f511588eca452d66819b7ea2b47537
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-01-21 20:50:22 +0200

    transform comma to period

--- src/components/DeliveryFeeCalculator.tsx
+++ src/components/DeliveryFeeCalculator.tsx
@@ -8,25 +8,42 @@ import { DeliveryFeeValues } from '../Types';
 const DeliveryFeeCalculator = () => {
   const [deliveryFee, setDeliveryFee] = useState<number>(0);
 
+  // Validation schema for the form
   const validationSchema = yup.object().shape({
     cartValue: yup
       .number()
       .transform((value, originalValue) => {
-        return (originalValue === '' ? undefined : value);
+        if (originalValue === '') {
+          return undefined;
+        } else if (String(originalValue).includes(',')) {
+          return Number(originalValue.replace(/,/g, '.'));
+        } else {
+          return value;
+        }
+        // return (originalValue === '' ? undefined : value);
       })
       .typeError('Value must be a number')
       .required('Cart value is required'),
     deliveryDistance: yup
       .number()
+      .transform((v, o) => {
+        return (o === '' ? undefined : v);
+      })
       .typeError('Value must be a number')
       .required('Delivery Distance is required'),
     items: yup
       .number()
+      .transform((v, o) => {
+        return (o === '' ? undefined : v);
+      })
       .typeError('Value must be a number')
       .required('Amount of items in cart is required'),
-    orderTime: yup.string().required('Time of order is required'),
+    orderTime: yup
+      .string()
+      .required('Time of order is required'),
   });
 
+  // React Hook Form components
   const { 
     register, 
     handleSubmit, 
@@ -52,22 +69,22 @@ const DeliveryFeeCalculator = () => {
         <div className="delivery-fee-form-field">
           <label>Cart Value</label>
           <input {...register('cartValue')} type="text" />
-          <div>{errors.cartValue && <span>{errors.cartValue.message}</span>}</div> 
+          {errors.cartValue && <div><span>{errors.cartValue?.message}</span></div>}
         </div>
         <div className="delivery-fee-form-field">
           <label>Delivery Distance</label>
           <input {...register('deliveryDistance', { required: true })} type="number" />
-          {errors.deliveryDistance && <span>Delivery distance is required</span>}
+          {errors.deliveryDistance && <div style={{ color: 'red' }}><span>{errors.deliveryDistance?.message}</span></div>}
         </div>
         <div className="delivery-fee-form-field">
           <label>Amount of Items</label>
           <input {...register('items', { required: true })} type="number" />
-          {errors.items && <span>Amount of items is required</span>}
+          {errors.items && <div><span>{errors.items?.message}</span></div>}
         </div>
         <div className="delivery-fee-form-field">
           <label>Time</label>
           <input {...register('orderTime', { required: true })} type="string" />
-          {errors.orderTime && <span>Cart Value is required</span>}
+          {errors.orderTime && <div><span>{errors.orderTime?.message}</span></div>}
         </div>
         <div className="delivery-fee-form-submit">
           <input type="submit" />
